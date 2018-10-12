91dc8775bbe732fc8d3d2e3a2bf2f7a211792917
Thorgaa <kozlowk22@wp.pl>
2018-10-12 22:04:02 +0200

    Wykład

---
+++ src/test/java/guru/springframework/stgpetclinic/StgPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.stgpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
